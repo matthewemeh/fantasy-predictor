057e2fc96b0d6375803a265c5f0e7b1aa335b0d0
matthewemeh <mcmekus10@gmail.com>
2022-10-10 17:05:14 +0100

    removed modal display

--- Pages/ConnectionErrorPage.js
+++ Pages/ConnectionErrorPage.js
@@ -1,52 +1,72 @@
-import React from 'react';
-import { View, StyleSheet, Text, Modal } from 'react-native';
-import { findFontSize } from '../utilities';
-import { colors } from '../constants';
+import React, { memo, useRef, useEffect } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { View as AnimatableView } from 'react-native-animatable';
+
 import Button from '../components/Button';
 
-function ConnectionErrorPage({ visible, command }) {
+import { colors } from '../constants';
+import { findFontSize } from '../utilities';
+
+const ConnectionErrorPage = ({ visible, command }) => {
+  const viewRef = useRef();
+  const initialRender = useRef(true);
+
+  const animations = {
+    slideIn: { from: { bottom: '-10%' }, to: { bottom: '0%' } },
+    slideOut: { from: { bottom: '0%' }, to: { bottom: '-10%' } },
+  };
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    if (visible) viewRef.current.animate(animations.slideIn);
+    else viewRef.current.animate(animations.slideOut);
+  }, [visible]);
+
   return (
-    <Modal visible={visible} transparent={true} animationType={'slide'}>
-      <View style={styles.mainView}>
-        <View style={styles.errorBarView}>
-          <Text allowFontScaling={false} style={styles.textStyle}>
-            No Internet Connection
-          </Text>
-          <Button
-            enabled={true}
-            command={command}
-            buttonColor={colors.primary}
-            buttonTextColor={colors.secondary}
-            buttonText={'Retry'}
-            extraStyles={{ width: '25%', height: '65%' }}
-            extraTextStyles={{ fontSize: findFontSize(15), fontFamily: 'PoppinsRegular' }}
-          />
-        </View>
-      </View>
-    </Modal>
+    <AnimatableView duration={400} ref={viewRef} style={styles.errorBarView}>
+      <Text allowFontScaling={false} style={styles.textStyle}>
+        No Internet Connection
+      </Text>
+
+      <Button
+        enabled={true}
+        command={command}
+        buttonText='Retry'
+        buttonColor={colors.primary}
+        buttonTextColor={colors.secondary}
+        extraStyles={{ width: '25%', height: '65%' }}
+        extraTextStyles={{ fontSize: findFontSize(15), fontFamily: 'PoppinsRegular' }}
+      />
+    </AnimatableView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   mainView: { width: '100%', height: '100%', justifyContent: 'flex-end' },
   errorBarView: {
-    flexDirection: 'row',
-    width: '100%',
+    zIndex: 2,
     height: '8%',
-    paddingHorizontal: '3%',
+    width: '100%',
+    bottom: '-10%',
+    position: 'absolute',
+    flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: '3%',
     justifyContent: 'space-between',
     backgroundColor: colors.secondary,
   },
   textStyle: {
     width: '75%',
     height: '100%',
-    fontSize: findFontSize(11),
-    textAlign: 'left',
-    textAlignVertical: 'center',
     color: colors.white,
+    fontSize: findFontSize(11),
+    textAlignVertical: 'center',
     fontFamily: 'PoppinsRegular',
   },
 });
 
-export default React.memo(ConnectionErrorPage);
+export default memo(ConnectionErrorPage);
