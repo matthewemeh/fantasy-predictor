6107826c62ef88a39831fd29bc4778738164fd90
matthewemeh <mcmekus10@gmail.com>
2022-12-20 07:22:55 +0100

    coupled connectionError page and loading page

--- Pages/LoadingPage.js
+++ Pages/LoadingPage.js
@@ -1,14 +1,15 @@
 import React, { memo } from 'react';
-import { View, Modal, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 import { colors } from '../constants';
 
-const LoadingPage = () => (
-  <Modal visible={true} transparent={true}>
-    <View style={styles.mainView}>
-      <Image source={require('../assets/loading.gif')} />
-    </View>
-  </Modal>
+import ConnectionErrorPage from './ConnectionErrorPage';
+
+const LoadingPage = ({ connectionErrorState, command }) => (
+  <View style={styles.mainView}>
+    <Image source={require('../assets/loading.gif')} />
+    <ConnectionErrorPage visible={connectionErrorState} command={command} />
+  </View>
 );
 
 const styles = StyleSheet.create({
