963c64bf14fe5d5baca2792bdf671b8104d3b8eb
matthewemeh <mcmekus10@gmail.com>
2023-03-21 14:00:13 +0100

    updated fantasy and scout screen

--- screens/FantasyScreen.js
+++
@@ -1,4 +0,0 @@
-import React, { memo } from 'react';
-import PlayerScreen from './PlayerScreen';
-const FantasyScreen = ({ visible }) => (<PlayerScreen type='fantasy' visible={visible}/>);
-export default memo(FantasyScreen);
--- screens/ScoutScreen.js
+++
@@ -1,4 +0,0 @@
-import React, { memo } from 'react';
-import PlayerScreen from './PlayerScreen';
-const ScoutScreen = ({ visible }) => (<PlayerScreen type='scout' visible={visible}/>);
-export default memo(ScoutScreen);
